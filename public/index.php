d7e8f2d8b267759029d1f025ed4ad2eeb07f3ace
nino.mauuary <nino.mauuary@gmail.com>
2025-01-27 12:41:10 +0100

    create the project structure, install jquery and bootstrap

---
+++ public/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en-US" class="no-js">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title></title>
+        <meta name="GamesFinder" content="Your site description." />
+        <link href="/node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="assets/css/main.css" media="all">
+        <script src="assets/js/scripts.js" async defer></script>
+        
+        <!-- Social Meta Tags -->
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="" />
+        <meta property="og:description" content="" />
+        <meta property="og:url" content="" />
+        <meta property="og:image" content="images/hom-banner-compressed.jpg" />
+        
+        <!-- Twitter Meta Tags -->
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="" />
+        <meta name="twitter:description" content="" />
+        <meta name="twitter:image" content="images/hom-banner-compressed.jpg" />
+        
+        <!-- Favicons -->
+        <link rel="icon" href="images/icon/cropped-favicon-32x32.png" sizes="32x32" />
+        <link rel="apple-touch-icon" href="images/icon/cropped-favicon-180x180.png" />
+        
+        <!-- Search Engine Optimization -->
+        <meta name="robots" content="index, follow" />
+        <link rel="canonical" href="" />
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please upgrade your browser for a better experience.</p>
+        <![endif]-->
+        <p>test</p>
+    </body>
+</html>
