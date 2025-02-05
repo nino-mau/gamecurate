9e94d42deb07cacafa63eab2592c666356241b7f
nino <nino.mauuary@gmail.com>
2025-02-05 01:20:24 +0100

    Update readme file to show project structure

---
+++ src/php/games-list-actions.php
@@ -0,0 +1,49 @@
+<?php 
+require_once 'db-functions.php';
+
+$gamesList = getDbTable('games');
+
+// Create empty array to store table columns
+$nameArr = [];
+$descArr = [];
+$genreArr = [];
+$priceArr = [];
+$reviewArr = [];
+$imagepathArr = [];
+
+// Store table infos in arrays representing columns 
+foreach ($gamesList as $dict) {
+    foreach ($dict as $key => $item) {
+        switch ($key) {
+            case $key === "name":
+                $nameArr[] = $item;
+                break;
+            case $key === "description":
+                $descArr[] = $item;
+                break;
+            case $key === "genre":
+                $genreArr[] = $item;
+                break;            
+            case $key === "price":
+                $priceArr[] = $item;
+                break;            
+            case $key === "review":
+                $reviewArr[] = $item;
+                break;            
+            case $key === "image_path":
+                $imagepathArr[] = $item;
+                break;
+        }
+    }
+}
+
+// Put these arrays back in a dictionnary
+$gamesDict = [];
+array_push($gamesDict, $nameArr, $descArr, $genreArr, $priceArr, $reviewArr, $imagepathArr);
+
+
+
+
+
+
+
