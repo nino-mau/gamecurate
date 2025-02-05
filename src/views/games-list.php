bdfb20ba7668071873bfbbf8b6d9d291423fdf5f
nino <nino.mauuary@gmail.com>
2025-02-06 00:46:06 +0100

    feat(games-list): -  Implement php script that dynamically display games present in the db on the page. -  Add a star icon next to the review numbers. -  Implement php script that dynamically assign a color of the review numbers and star icon depending on how high the score is. - Some general UI changes on the games list page to improve the UI

--- src/views/games-list.php
+++ src/views/games-list.php
@@ -1,9 +1,100 @@
 <?php
 include $ROOT.'/games-finder/src/includes/header.php';
+
+$gamesDict = $_SESSION['gamesDict'];
+
+$name = $gamesDict[0];
+$desc = $gamesDict[1];
+$genre = $gamesDict[2];
+$price = $gamesDict[3];
+$review = $gamesDict[4];
+$img_path = $gamesDict[5];
+
+function handleReviewColor($reviewScore) {
+    if ($reviewScore >= 90) {
+        $reviewHtml = "<span class=\"game-review-cl--green\"><i class=\"review-icon ph-fill ph-star\"></i> ";
+    }
+    else if ($reviewScore > 70) {
+        $reviewHtml = "<span class=\"game-review-cl--lightgreen\"><i class=\"review-icon ph-fill ph-star\"></i> ";
+    }    
+    else if ($reviewScore >= 50) {
+        $reviewHtml = "<span class=\"game-review-cl--lightgreen\"><i class=\"review-icon ph-fill ph-star\"></i> ";
+    } else {
+        $reviewHtml = "<span class=\"game-review-cl--red\"><i class=\"review-icon ph-fill ph-star\"></i> ";
+    }
+    $res = $reviewHtml . $reviewScore . '/100</span>';
+    return $res;
+}
+
+function test() {
+    echo 'test';
+}
+
+$gamesAmount = count($gamesDict[0]);
+
 ?>
 
 <main>
     <div class="games-grid container text-center">
+    <?php
+        $displayReview = 'handleReviewColor';
+        $i = 0; 
+        while ($i < $gamesAmount) {
+            $i2 = $i + 1;
+            $i3 = $i + 2;
+            $reviewHtml1 = handleReviewColor($review[$i]);
+            $reviewHtml2 = handleReviewColor($review[$i2]);
+            $reviewHtml3 = handleReviewColor($review[$i3]);
+            echo 
+            <<<HTML
+                <div class="row row-cols-3">
+                    <div class="games-grid-containers col">
+                        <img class="game-img shadow rounded " src="$img_path[$i]">
+                        <div class="game-grid-sub-containers d-flex flex-row justify-content-between">
+                            <p class="game-review">$reviewHtml1 </p>
+                            <!-- <div class="col game-price"><p>$price[$i]$</p></div> -->
+                            <p class="game-genre">$genre[$i]</p>
+                        </div>
+                        <!-- <p class="game-titles">$name[$i]</p> -->
+                        <!-- <p class="game-genre">$genre[$i]</p> -->
+                    </div>
+                    <div class="games-grid-containers col">
+                        <img class="game-img shadow rounded " src="$img_path[$i2]">
+                        <div class="game-grid-sub-containers d-flex flex-row justify-content-between">
+                            <p class="game-review">$reviewHtml2 </p>
+                            <!-- <div class="col game-price"><p>$price[$i2]$</p></div> -->
+                            <p class="game-genre">$genre[$i2]</p>
+                        </div>
+                        <!-- <p class="game-titles">$name[$i2]</p> -->
+                        <!-- <p class="game-genre">$genre[$i2]</p> -->
+                    </div>
+                    <div class="games-grid-containers col">
+                        <img class="game-img shadow rounded " src="$img_path[$i3]">
+                        <div class="game-grid-sub-containers d-flex flex-row justify-content-between">
+                            <!-- <div class="col game-price"><p>$price[$i3]$</p></div> -->
+                            <p class="game-review">$reviewHtml3 </p>
+                            <p class="game-genre">$genre[$i3]</p>
+                        </div>
+                        <!-- <p class="game-titles">$name[$i3]</p> -->
+                        <!-- <p class="game-genre">$genre[$i3]</p> -->
+                    </div>
+                </div>            
+            HTML;
+            $i += 3;
+        }
+    ?>
+    </div>
+</main>
+
+
+
+
+
+
+
+
+
+    <!-- <div class="games-grid container text-center">
         <div class="row row-cols-3">
             <div class="games-grid-containers col">
                 <img class="game-img" src="/games-finder/public/assets/img/games/the-witcher-3-wild-hunt.jpeg">
@@ -83,7 +174,7 @@ include $ROOT.'/games-finder/src/includes/header.php';
             </div>
         </div>
     </div>
-</main>
+</main> -->
 
 
 <?php
