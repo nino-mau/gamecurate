2208ba35ccd8ee3c81bad630f4869760f6f08477
nino <nino.mauuary@gmail.com>
2025-02-09 19:10:23 +0100

    feat: Implement the footer includes and update it's style

--- src/includes/footer.php
+++ src/includes/footer.php
@@ -1,6 +1,70 @@
-   </body>
+<footer class="text-center text-lg-start text-white" style="background-color: #1c2331">
+    <section class="bg-primary d-flex justify-content-between p-4">
+        <div class="me-5">
+            <span>Get connected with us on social networks:</span>
+        </div>
+        <div role="list" class="footer-socials">
+            <a href="" class="icons text-white me-4">
+                <img class="svg hvr-float" src="/games-finder/public/assets/img/icons/socials/logo-facebook.svg">
+            </a>
+            <a href="" class="icons text-white me-4">
+                <img class="svg hvr-float" src="/games-finder/public/assets/img/icons/socials/logo-twitter.svg">
+            </a>
+            <a href="" class="icons text-white me-4">
+                <img class="svg hvr-float" src="/games-finder/public/assets/img/icons/socials/logo-github.svg">
+            </a>
+            <a href="" class="icons text-white me-4">
+                <img class="svg hvr-float" src="/games-finder/public/assets/img/icons/socials/logo-instagram.svg">
+            </a>
+            <a href="" class="icons text-white me-4">
+                <img class="svg hvr-float" src="/games-finder/public/assets/img/icons/socials/logo-linkedin.svg">
+            </a>
+            <a href="" class="icons text-white me-4">
+                <img class="svg hvr-float" src="/games-finder/public/assets/img/icons/socials/logo-youtube.svg">
+            </a>
+        </div>
+    </section>
+    <section class="footer-middle-div">
+        <div class="containerDiv container text-center text-md-start h-100">
+            <div class="row h-100">
+                <div class="footer-site-icon col-md-3 col-lg-4 col-xl-3 mx-auto d-flex justify-content-center align-items-center">
+                    <a class="link" href="/games-finder/public/index.php"><img class="svg" src="/games-finder/public/assets/img/icons/site-icon.svg"></a>
+                </div>
+                <div class="content-div col-md-2 col-lg-2 col-xl-2 mx-auto d-flex flex-column justify-content-center gap-3 align-items-start">
+                    <h6 class="title text-uppercase fw-bold m-0">Used Frameworks</h6>
+                    <hr class="line mb-4 mt-0 d-inline-block" />
+                    <a href="https://getbootstrap.com/" class="link">Bootstrap</a>
+                    <a href="https://popper.js.org/docs/" class="link">Popper.js</a>
+                    <a href="https://ianlunn.github.io/Hover/" class="link">Hover.css</a>
+                    <a href="https://jquery.com/" class="link">Jquery</a>
+                </div>
+                <div class="content-div col-md-3 col-lg-2 col-xl-2 mx-auto d-flex flex-column justify-content-center gap-3 align-items-start">
+                    <h6 class="title text-uppercase fw-bold m-0">Useful links</h6>
+                    <hr class="line mb-4 mt-0 d-inline-block" />
+                    <a href="/games-finder/src/views/register.php" class="link">Register</a>
+                    <a href="/games-finder/src/views/login.php" class="link">Login</a>
+                    <a href="/games-finder/src/views/contact.php" class="link">Contact</a>
+                    <a href="/games-finder/src/views/about.php" class="link">About</a>
+                </div>
+                <div class="content-div col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 d-flex flex-column justify-content-center gap-3 align-items-start">
+                    <h6 class="title text-uppercase fw-bold m-0">Contact</h6>
+                    <hr class="line mb-4 mt-0 d-inline-block" />
+                    <p class="txt"><img class="svg" src="/games-finder/public/assets/img/icons/contact/home-icon.svg"> 36 Rue de Placeholder, 38240</p>
+                    <p class="txt"><img class="svg" src="/games-finder/public/assets/img/icons/contact/mail-icon.svg"> nino.placeholder@gmail.com</p>
+                    <p class="txt"><img class="svg" src="/games-finder/public/assets/img/icons/contact/call-icon.svg"> + 01 234 567 88</p>
+                    <p class="txt"><img class="svg" src="/games-finder/public/assets/img/icons/contact/print-icon.svg"> + 01 234 567 89</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="footer-bottom-div p-3 d-flex align-items-center justify-content-center">
+        <p class="m-0">© 2024 Copyright: GamesFinder.com</p>
+    </div>
+</footer>
+</body>
+
 </html>
 
-<?php 
+<?php
 // $_SESSION['username'] = '';
 ?>
